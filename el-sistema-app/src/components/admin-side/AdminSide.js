3fd682de22727a844837e20716260c269f049616
Lloyd Grubham <grubham@gmail.com>
2017-06-25 00:16:21 -0600

    First draft of admin navbar/sidebar and the dashboard styling.

--- el-sistema-app/src/components/admin-side/AdminSide.js
+++ el-sistema-app/src/components/admin-side/AdminSide.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
-import {Route, Switch} from 'react-router-dom';
+// import {Route, Switch} from 'react-router-dom';
 import Header from './app-frame/Header';
-import Dashboard from './components/Dashboard/Dashboard';
-import Volunteers from './components/Volunteers/Volunteers';
-import Performances from './components/Performances/Performances';
-import Classes from './components/Classes/Classes';
-import ContentPictures from './components/ContentPictures/ContentPictures';
-import ContentText from './components/ContentText/ContentText';
-import Media from './components/Media/Media';
-import Blog from './components/Blog/Blog';
+// import Dashboard from './components/Dashboard/Dashboard';
+// import Volunteers from './components/Volunteers/Volunteers';
+// import Performances from './components/Performances/Performances';
+// import Classes from './components/Classes/Classes';
+// import ContentPictures from './components/ContentPictures/ContentPictures';
+// import ContentText from './components/ContentText/ContentText';
+// import Media from './components/Media/Media';
+// import Blog from './components/Blog/Blog';
 
-
-class AdminSide extends Component {
+export default class AdminSide extends Component {
   render() {
     return (
       <div className="container">
         <Header />
-        <Switch>
+        {/*<Switch>
           <Route path='/admin/volunteers' component={Volunteers} />
           <Route path='/admin/classes' component={Classes} />
           <Route path='/admin/performances' component={Performances} />
@@ -25,10 +24,8 @@ class AdminSide extends Component {
           <Route path='/admin/media' component={Media} />
           <Route path='/admin/blog' component={Blog} />
           <Route path='/admin' component={Dashboard} />
-        </Switch>
+        </Switch>*/}
       </div>
     )
   }
 }
-
-export default AdminSide;
