a33fbfb85cb9035dca478a7f00ff2c98ff2f2f65
yourGitHubusername <yourGitHub@email.com>
2017-06-25 05:34:21 -0600

    volunteer-table

--- el-sistema-app/src/components/admin-side/components/Volunteers/Volunteers.js
+++ el-sistema-app/src/components/admin-side/components/Volunteers/Volunteers.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import AddVolunteer from './components/Add-Volunteer';
+import VolunteerTable from './components/Volunteer-Table'
 
 class Volunteers extends Component {
   render() {
     return (
         <div>
-
       <AddVolunteer />
+      <VolunteerTable />
       </div>
     )
   }
