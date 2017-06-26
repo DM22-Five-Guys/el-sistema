412d448350b942ec4d5143137a4717336b6795d7
Lloyd Grubham <grubham@gmail.com>
2017-06-26 15:02:19 -0600

    Restructured admin routing

--- el-sistema-app/src/components/admin-side/AdminSide.js
+++ el-sistema-app/src/components/admin-side/AdminSide.js
@@ -1,30 +1,60 @@
 import React, { Component } from 'react';
-// import {Route, Switch} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Header from './app-frame/Header';
-// import Dashboard from './components/Dashboard/Dashboard';
-// import Volunteers from './components/Volunteers/Volunteers';
-// import Performances from './components/Performances/Performances';
-// import Classes from './components/Classes/Classes';
-// import ContentPictures from './components/ContentPictures/ContentPictures';
-// import ContentText from './components/ContentText/ContentText';
-// import Media from './components/Media/Media';
-// import Blog from './components/Blog/Blog';
+import Sidebar from './app-frame/Sidebar';
+
+import './app-frame/style.css';
+
+import Dashboard from './components/Dashboard/Dashboard';
+import Volunteers from './components/Volunteers/Volunteers';
+import Performances from './components/Performances/Performances';
+import Classes from './components/Classes/Classes';
+import ContentPictures from './components/ContentPictures/ContentPictures';
+import ContentText from './components/ContentText/ContentText';
+import Media from './components/Media/Media';
+import Blog from './components/Blog/Blog';
 
 export default class AdminSide extends Component {
+  constructor(){
+    super();
+    this.state={
+      showSide: false
+    }
+
+    this.toggleSide = this.toggleSide.bind(this);
+  }
+
+  toggleSide(){
+    this.setState({
+      showSide: !this.state.showSide
+    })
+  }
+
   render() {
     return (
       <div className="container">
-        <Header />
-        {/*<Switch>
-          <Route path='/admin/volunteers' component={Volunteers} />
-          <Route path='/admin/classes' component={Classes} />
-          <Route path='/admin/performances' component={Performances} />
-          <Route path='/admin/content/pictures' component={ContentPictures} />
-          <Route path='/admin/content/text' component={ContentText} />
-          <Route path='/admin/media' component={Media} />
-          <Route path='/admin/blog' component={Blog} />
-          <Route path='/admin' component={Dashboard} />
-        </Switch>*/}
+        <Header toggleSide={this.toggleSide}/>
+        <div className="all-content">
+          {
+            this.state.showSide
+            ?
+            <Sidebar />
+            :
+            null
+          }
+            <section className="content">
+              <Switch>
+                <Route path='/admin/volunteers' component={Volunteers} />
+                <Route path='/admin/classes' component={Classes} />
+                <Route path='/admin/performances' component={Performances} />
+                <Route path='/admin/content/pictures' component={ContentPictures} />
+                <Route path='/admin/content/text' component={ContentText} />
+                <Route path='/admin/media' component={Media} />
+                <Route path='/admin/blog' component={Blog} />
+                <Route path='/admin' component={Dashboard} />
+              </Switch>
+            </section>
+        </div>
       </div>
     )
   }
