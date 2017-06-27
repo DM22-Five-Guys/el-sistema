d4c8fba279ca25e1a95a04d46059250b5d63116c
Brooks Benson <brooksbenson01@gmail.com>
2017-06-27 17:43:13 -0600

    made improvements to user side app-frame, partially built footer, added styling

--- el-sistema-app/src/components/user-side/app-frame/ContentContainer.js
+++ el-sistema-app/src/components/user-side/app-frame/ContentContainer.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 
 export default class ContentContainer extends Component {
 
-  constructor() {
-    this.state({
-      topMargin: null
-    })
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      marginTop: props.marginTop
+    }
   }
 
   componentWillReceiveProps(props) {
+    this.setState({
+      marginTop: props.marginTop
+    })
+  }
 
+  render() {
+    return (
+      <section className="UserSide-content" style={{marginTop: this.state.marginTop}}>
+        {this.props.children}
+      </section>
+    )
   }
 }
---
+++ el-sistema-app/src/components/user-side/app-frame/assets/footerContent.js
@@ -0,0 +1,4 @@
+export default {
+  stay_connected: "Enter your mobile number to start receiving text message notifications from El Sistema Pittsburgh! Unsubscribe at anytime.",
+  committed: "El Sistema Pittsburgh has successfully developed a diverse, qualified, and competent leadership committee focused on ensuring artistic and musical education. In part, a highly unique curriculum has been developed to meet the distinct needs of community residents while alleviating the current budget void that has transversely impacted public schools’ art and music programs."
+}
