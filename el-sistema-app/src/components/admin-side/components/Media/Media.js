b257d1d984d9323994107c1fda2dd4cbf9e6126a
yourGitHubusername <yourGitHub@email.com>
2017-06-27 23:42:04 -0600

    pointless-change

--- el-sistema-app/src/components/admin-side/components/Media/Media.js
+++ el-sistema-app/src/components/admin-side/components/Media/Media.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import '../style.css';
+import './style.css';
 import trash from '../../../../../img/trash.png';
 import edit from '../../../../../img/pencil.png';
 
