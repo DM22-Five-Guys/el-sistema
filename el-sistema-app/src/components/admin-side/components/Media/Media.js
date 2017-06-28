5a8b12ff295aa3434431f4f0640898ccb7021dee
yourGitHubusername <yourGitHub@email.com>
2017-06-28 14:33:00 -0600

    media-update

---
+++ el-sistema-app/src/components/admin-side/components/Media/Media.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import MediaTable from './components/Media-Table';
+
+class Media extends Component{
+  render(){
+    return(
+      <MediaTable />
+    )
+  }
+}
+export default Media;
