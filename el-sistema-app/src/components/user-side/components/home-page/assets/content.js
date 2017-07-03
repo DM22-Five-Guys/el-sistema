446b0dc0377bd5f4b40797d4f0d7b400c1bbbc1b
Brooks Benson <brooksbenson01@gmail.com>
2017-07-03 16:54:40 -0600

    home-page update

---
+++ el-sistema-app/src/components/user-side/components/home-page/assets/content.js
@@ -0,0 +1,18 @@
+export default {
+  hero1: {
+    quote: "From the minute a child is taught to play an instrument, he’s no longer poor. He is a child in progress who will become a citizen.",
+    author: "Dr. Jose Antonio Abreu",
+    authorTitle: "Founder of Venezuela’s El Sistema"
+  },
+  hero2: {
+    about: "El Sistema Pittsburgh provides Pittsburgh’s youth with accessible and intensive classical music instruction. Utilizing music as a vehicle for positive change, ESPGH promotes the development of music, cognitive and social skills, self-esteem, and community pride in youth."
+  },
+  infoGraphics: {
+    visionCard: "Over time, El Sistema Pittsburgh will create a network of El Sistema-inspired núcleos, or community locations, throughout the city and the region. Each will have a unique, community-based approach to realizing core values at the heart of our mission. Every child in ESPGH will be treated as a \"key player.\" The orchestra will grow as a tight-knit community devoted to cultivating responsible citizens within and beyond the orchestra.",
+    donationCard: "El Sistema Pittsburgh is grateful for our family of supporters. Partnerships with individual donors, corporations, government agencies, and foundations ensure that music education programs are available to young musicians in every neighborhood, regardless of socio-economic circumstance. Every dollar donated to ESPGH helps change kids’ lives through music.",
+    registrationCard: {
+      howTo: "To sign up for El Sistema’s after-school program, registration forms are available for pickup at the Pittsburgh Public Schools.",
+      fyi: "For all students outside of the school district, they are able to join with a tuition cost."
+    }
+  }
+}
--- el-sistema-app/src/components/user-side/components/home-page/assets/hero1quote.js
+++
@@ -1,5 +0,0 @@
-export default {
-  quote: "From the minute a child is taught to play an instrument, he’s no longer poor. He is a child in progress who will become a citizen.",
-  author: "Dr. Jose Antonio Abreu",
-  authorTitle: "Founder of Venezuela’s El Sistema"
-}
