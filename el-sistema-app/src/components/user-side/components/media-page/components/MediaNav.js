3dfa304616c414d0baabd03bafed6ad5c20852be
yourGitHubusername <yourGitHub@email.com>
2017-07-07 09:09:10 -0600

    picture updates

--- el-sistema-app/src/components/user-side/components/media-page/components/MediaNav.js
+++ el-sistema-app/src/components/user-side/components/media-page/components/MediaNav.js
@@ -16,7 +16,13 @@ import Image12 from '../../../../../img/Penguins.png';
 import Image13 from '../../../../../img/violinKid.jpg';
 import Image14 from '../../../../../img/love.jpg';
 import Image15 from '../../../../../img/hair.jpeg';
-
+import pitts from '../../../../../img/Pitts.jpg';
+import music from '../../../../../img/Music.png';
+import violinKid from '../../../../../img/violinKid.jpg';
+import venezuela from '../../../../../img/venezuela.jpg';
+import conduct from '../../../../../img/conduct.jpg';
+import devmtn from '../../../../../img/maxresdefault.jpg';
+import sistema from '../../../../../img/elsistema.jpg';
 import shareButton from '../../../../../img/share.png';
 import loadPhotos from '../../../../../img/read-more-button.png';
 import backToTop from '../../../../../img/return-to-top.png';
@@ -45,35 +51,35 @@ class MediaNav extends Component {
             <li className='media-block-list-one'><div className='media-blocks  col-md-2'>
 <img src={Image1} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>3 Days</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-two'><div className='media-blocks col-md-2'>
 <img src={Image2} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>6 Days</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-three'><div className='media-blocks col-md-2'>
 <img src={Image3} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>7 Days</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-four'><div className='media-blocks col-md-2'>
 <img src={Image4} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>10 Days</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-five'><div className='media-blocks col-md-2'>
 <img src={Image5} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>23 Days</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
@@ -82,21 +88,21 @@ class MediaNav extends Component {
             <li  className='media-block-list-one'><div className='media-blocks col-md-4'>
 <img src={Image6} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>25 Days</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-two'><div className='media-blocks col-md-4'>
 <img src={Image7} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>29 Days</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-three'><div className='media-blocks col-md-4'>
 <img src={Image8} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>1 Month</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
@@ -119,87 +125,87 @@ class MediaNav extends Component {
           <li className='media-block-list-two'><div className='media-blocks col-md-2'>
 <img src={Image11} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>2 months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-three'><div className='media-blocks col-md-2'>
 <img src={Image12} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>2 months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-four'><div className='media-blocks col-md-2'>
 <img src={Image13} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>2 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-five'><div className='media-blocks col-md-2'>
 <img src={Image14} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>3 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           </ul>
           <ul className='four-row-block'>
             <li className='media-block-list-one'><div className='media-blocks col-md-4'>
-<img src={Image1} className='media-picture'></img>
+<img src={pitts} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>3 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-two'><div className='media-blocks col-md-4'>
-<img src={Image1} className='media-picture'></img>
+<img src={music} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>3 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-three'><div className='media-blocks col-md-4'>
-<img src={Image1} className='media-picture'></img>
+<img src={sistema} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>4 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-four'><div className='media-blocks col-md-4'>
-<img src={Image1} className='media-picture'></img>
+<img src={conduct} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>4 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
               </div></li>
           </ul>
           <ul className='five-row-block'>
             <li className='media-block-list-one'><div className='media-blocks col-md-2'>
-      <img src={Image1} className='media-picture'></img>
+      <img src={venezuela} className='media-picture'></img>
       <ul className='media-block-header'>
-        <li>1 month</li>
+        <li>4 Months</li>
         <li><img src={shareButton} className='share-img'></img></li>
       </ul>
             </div>
           </li>
           <li className='media-block-list-two'><div className='media-blocks col-md-2'>
-<img src={Image2} className='media-picture'></img>
+<img src={violinKid} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>4 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-three'><div className='media-blocks col-md-2'>
-<img src={Image2} className='media-picture'></img>
+<img src={devmtn} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>5 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-four'><div className='media-blocks col-md-2'>
-<img src={Image2} className='media-picture'></img>
+<img src={music} className='media-picture'></img>
 <ul className='media-block-header'>
   <li>1 month</li>
   <li><img src={shareButton} className='share-img'></img></li>
@@ -208,7 +214,7 @@ class MediaNav extends Component {
           <li className='media-block-list-five'><div className='media-blocks col-md-2'>
 <img src={Image2} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>5 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
 
@@ -218,28 +224,28 @@ class MediaNav extends Component {
             <li className='media-block-list-one'><div className='media-blocks col-md-4'>
 <img src={Image2} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>6 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-two'><div className='media-blocks col-md-4'>
 <img src={Image2} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>6 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-three'><div className='media-blocks col-md-4'>
 <img src={Image2} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>7 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
             </div></li>
           <li className='media-block-list-four'><div className='media-blocks col-md-4'>
 <img src={Image2} className='media-picture'></img>
 <ul className='media-block-header'>
-  <li>1 month</li>
+  <li>7 Months</li>
   <li><img src={shareButton} className='share-img'></img></li>
 </ul>
               </div></li>
