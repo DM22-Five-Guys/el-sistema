0b203177be29b8e91aee7f8ac5d613f632a79cad
pchiarato <phillip.chiarato@gmail.com>
2017-07-07 03:38:43 -0600

    added styles to payment and donation view

--- el-sistema-app/src/components/user-side/components/donate-page/DonatePage.js
+++ el-sistema-app/src/components/user-side/components/donate-page/DonatePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DonateMoney from './components/DonateMoney';
+import DonateInstrument from './components/DonateInstrument';
 
 import './Donate-Page.style.css';
 
@@ -7,12 +8,33 @@ class DonatePage extends Component {
   render() {
     return (
       <div className="donate-page-container">
-          <h1 className="donate-title">Donate Page</h1>
+        <div className="donate-page-header">
+          <h1 className="donate-title">Support El Sistema Pittsburgh</h1>
+          <div className="donate-first-p">
+              <h4 className="donate-first-p-title">Our Goals</h4>
+              <p className="donate-first-paragraph">
+                  El Sistema Pittsburgh provides Pittsburgh’s youth with accessible and intensive classical music instruction. Utilizing music as a vehicle for positive change, ESPGH promotes the development of music, cognitive and social skills, self-esteem, and community pride in youth.
+              </p>
+              <h4 className="donate-second-p-title">How Donations are Used</h4>
+              <p className="donate-second-paragraph">
+                100% of all Donations are used to help fund El Systema’s materials, teachers, and concerts. Paypal takes a slight cut of 7% but the rest is distributed based on what is needed at the time. El Systema complies with all E32c regulations and takes great pride in it’s transparency. 
+              </p>
+          </div>
+          <div className="help-info">
+            <h4 className="help-text">3 Ways to Help:</h4>
+          </div>
+        </div>
+
+          
           <div className="donate-money-outter-container">
             <DonateMoney/>
+            <DonateInstrument/>
+          </div> {/* end of donate-money-outter-container */}
+          <div className="donate-instrument-outter-container">
+              
           </div>
             
-      </div>
+      </div> 
     )
   }
 }
---
+++ el-sistema-app/src/components/user-side/components/donate-page/components/DonateInstrument.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './DonateInstrument.css';
+
+export default function DonateInstrument(){
+    return(
+        <div>
+            <div className="donate-instrument-container">
+                <h1 className="donate-instrument-title">
+                    Donate an Instrument
+                </h1>
+                <h3 className="donate-instrument-subtitle">
+                    Call to schedule a drop-off
+                </h3>
+                <p className="donate-instrument-paragraph">
+                    6 Loop St, # 4
+                    Pittsburgh, Pennsylvania, PA 15215
+                    (801) 224 - 8970
+                </p>
+            </div>
+        </div>
+    )
+}
